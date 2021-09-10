c4f698b140996965baf5d64ebca7d59cf0688a3e
Bexse <89146106+Bexse@users.noreply.github.com>
2021-09-10 06:36:35 -0400

    Update W2D3 arrayMethods.js

--- CS305/W2D3 arrayMethods.js
+++ CS305/W2D3 arrayMethods.js
@@ -13,40 +13,13 @@ module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculato
     
           if (val < a || val > b) {
             arr.splice(i, 1);
-            i--;//why
+            i--;
           }
         }
       
       }
 
-
-
-    function Calculator() {
-
-        this.methods = {          "-": (a, b) => a - b,
-          "+": (a, b) => a + b
-        };
-      
-        this.calculate = function(str) {
-      
-          let split = str.split(' '),
-            a = +split[0],
-            op = split[1],
-            b = +split[2];
-      
-          if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-            return NaN;
-          }
-      
-          return this.methods[op](a, b);
-        };
-      
-        this.addMethod = function(name, func) {
-          this.methods[name] = func;
-        };
-      }
-
-  }
+  
 
   function unique(arr) {
 
