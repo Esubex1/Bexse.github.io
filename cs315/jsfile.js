2506f41bef23c83801cafb4dc59456e4dbd97ca8
Bexse <89146106+Bexse@users.noreply.github.com>
2021-09-29 19:15:51 -0400

    Update jsfile.js

--- cs315/jsfile.js
+++ cs315/jsfile.js
@@ -1,1 +1,40 @@
+'use strict';
+function clickToAdd(){
+    let input = document.getElementById("input");
+    let result = input.value;
+    let outPut = document.getElementById("secInput");
+    outPut.value = result;
+   input.value = "";
+            }
+
+function addition(){
+ let fVal = document.getElementById("firstValue");
+ let sVal = document.getElementById("secondValue");
+ let result = Number(fVal.value) + Number(sVal.value);
+ alert(result);
+ let p = document.createElement("p");
+ p.innerHTML = result;
+ document.body.append(p);
+
+}
+
+function multiplication(){
+ let fVal =document.getElementById("firstValue");
+ let sVal =document.getElementById("secondValue");
+ let result = Number(fVal.value) * Number(sVal.value);
+ alert(result);
+ let p = document.createElement("p");
+ p.innerHTML = result;
+ document.body.append(p);
+}
+
+function substraction(){
+ let fVal =document.getElementById("firstValue");
+ let sVal =document.getElementById("secondValue");
+ let result = Number(fVal.value) - Number(sVal.value);
+ alert(result);
+ let p = document.createElement("p");
+ p.innerHTML = result;
+ document.body.append(p);
+}
 
